f6d4f00ee78079787b2131ff471ec2e6afdabf4a
akhtaraliwarar <akhtarwarar1@gmail.com>
2024-09-22 10:42:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+export {};
+// const calculater:{
+//     num1:number
+//     num2 :number
+//     opreator:string
+//   }= await inquirer.prompt([
+//     {
+//     type:"number",
+//     name: "num1",
+//     message : "type a  1st number ",
+//     },
+//     {
+//       type:"number",
+//       name: "num2",
+//       message: "type a second number",
+//     },
+//     {
+//       type: "list",
+//       name : "opreator",
+//       message: "choose opreator",
+//       choices: ["+", "-", "*", "/" ]
+//     },
+//   ]);
+//   let {num1, opreator, num2 }= calculater;
+//   let result
+//   switch (opreator) {
+//       case "+" : result = num1 + num2
+//       break;
+//       case "_" : result = num1 - num2
+//       break;
+//       case "*": result = num1 * num2
+//       break;
+//       case "/" : result = num1 / num2
+//       break; 
+//      default:
+//       console.log("invalid msg");
+//   }
+//   // console.log(result);
+//   console.log(`${num1} ${opreator} ${num2}= ${result}`);
+//   console.log("whoes here eror");
